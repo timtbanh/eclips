84786fe72cd02b6142f44e35917dd9a9759ab420
Ming Dai <mingdai@ming.local>
2017-02-12 16:11:25 -0800

    basic server no models

---
+++ account/urls.py
@@ -0,0 +1,27 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^login/$', views.login, name='login'),
+]
+
+'''
+urlpatterns = patterns('',
+    
+
+    url(
+        r'^login/$',
+        'django.contrib.auth.views.login',
+        name='login',
+        kwargs={'template_name': 'accounts/login.html'}
+    ),
+    url(
+        r'^logout/$',
+        'django.contrib.auth.views.logout',
+        name='logout',
+        kwargs={'next_page': '/'}
+    ),
+)
+'''
