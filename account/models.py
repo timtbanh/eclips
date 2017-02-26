f096092dd9f9c669d45af9b27a0355aa220291e8
Ming Dai <mingdai@ming.local>
2017-02-26 14:48:25 -0800

    created django models and changed requiremtnts

--- account/models.py
+++ account/models.py
@@ -1,5 +1,55 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from datetime import datetime#
 
-# Create your models here.
+        
+@python_2_unicode_compatible
+class Barber(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    phone = models.IntegerField()
+    address = models.CharField(max_length=200, null=True)
+    price = models.TextField(max_length=200, null=True)#list all the services being offered and their prices
+    walkin = models.CharField(max_length=200, null=True)#walkin, goto customer, either
+    description = models.TextField(null=True) #other wanted information
+    avgRating = models.IntegerField(null=True)#averaged Rating
+    profilePic = models.CharField(max_length=200, null=True)#link to prof pic
+    schedule = models.TextField(null=True)#display schedule in form of text
+    def __str__(self):
+        return 'Barber Name: %s %s' % (self.first_name, self.last_name)
+
+@python_2_unicode_compatible
+class Client(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    phone = models.IntegerField()
+    address = models.CharField(max_length=200, null=True)
+    description = models.TextField(null=True) #other wanted information
+    avgRating = models.IntegerField(null=True)#averaged Rating
+    profilePic = models.CharField(max_length=200, null=True)#link to prof pic
+    def __str__(self):
+        return 'Client Name: %s %s' % (self.first_name, self.last_name)
+
+@python_2_unicode_compatible
+class Appointment(models.Model):
+    when = models.DateTimeField(default=datetime.now)
+    address = models.CharField(max_length=200)
+    barber = models.ForeignKey(Barber,on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    isPerformed = models.BooleanField(default=False)#if by date time, then true
+    def __str__(self):
+        return 'Appointment between: %s & %s @ %s' % (self.barber.first_name, self.client.last_name, self.when)
+
+@python_2_unicode_compatible
+class Review(models.Model):
+    comment = models.TextField()
+    rating = models.IntegerField()
+    appointment = models.ForeignKey(Appointment, null=True, on_delete=models.CASCADE)
+    def __str__(self):
+        return 'Review of %s' % (self.appointment)
